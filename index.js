cb7fc759dd0c73ef94025fe8291bd9abf7beb018
Kieran Chadwick <kieran.chadwick@outlook.com>
2021-03-02 23:42:16 +0000

    debug

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);
 
+client.on("debug", console.log);
 client.on("message", async (message) => {
   if (!message.guild) return;
 
